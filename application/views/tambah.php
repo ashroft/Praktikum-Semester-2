acdcfd017d4710155a8f33ca46e948fcdb07c1e9
ashroft <ashroft73@gmail.com>
2022-07-05 16:33:57 +0700

    add Praktikum 6

---
+++ application/views/tambah.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Siswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="home">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="daftar">Daftar Siswa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form action="<?php echo base_url('daftar') ?>" method="post">
+  <div class="form-group row">
+    <label for="nim" class="col-4 col-form-label">NIM</label> 
+    <div class="col-8">
+      <input id="nim" name="nim" placeholder="NIM" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">Nama</label> 
+    <div class="col-8">
+      <input id="nama" name="nama" placeholder="Nama" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="prodi" class="col-4 col-form-label">Program Studi</label> 
+    <div class="col-8">
+      <select id="prodi" name="prodi" class="custom-select">
+        <option value="si">Sistem Informasi</option>
+        <option value="ti">Teknik Informatika</option>
+        <option value="bd">Bisnis Digital</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai" class="col-4 col-form-label">Nilai</label> 
+    <div class="col-8">
+      <input id="nilai" name="nilai" placeholder="Nilai" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+  </body>
+</html>
